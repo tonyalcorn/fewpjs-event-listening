79bcbe6d401ba2a6d9542ae292a21667fd3d87aa
tonyalcorn <xmastony@gmail.com>
2020-11-23 16:37:37 -0800

    commit tree f9dbb31e79aa2d6088380ec0e85d951d7a634417

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ input.addEventListener('click', function(event) {
   alert('I was clicked!');
 });
 }
+
+const main = document.getElementById('main')
+input.addEventListener("DOMContentLoaded", function(event) {
+    alert("This is really cool.");
+  });
+  
+// const input = document.querySelector('input');
+
+// function addingEventListener() {
+//   input.addEventListener('click', function(event) {
+//     alert("clicked")
+//   });
+// }
